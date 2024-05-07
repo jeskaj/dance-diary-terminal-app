2d2af116cc84b743609e1033b101b269b279137e
jeskaj <jwjakeman@yahoo.com.au>
2024-05-07 15:52:33 +1000

    Completed function for adding new student data to students.json (while retaining existing data). Started building main menu in main.py.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,29 @@
 from student import Student, add_student, view_students
 
 
-add_student()
-# view_students()
+def main_menu():
+    print('\nPlease select from the following options:')
+    selection_1 = input(print('Syllabus: 1\nStudents: 2\nLessons: 3'))
+
+    if selection_1 == 1:
+        print('You have selected Syllabus.  This part of the program is yet to be written')
+        
+    elif selection_1 == 2:
+        print('\nPlease select from the following options:')
+        selection_2 = input(print('\nView student details: 1\nAdd a new student: 2'))
+            
+        if selection_2 == 1:
+            view_students()
+        elif selection_2 == 2:
+            add_student()
+        else:
+            print('Invalid selection')
+
+    elif selection_1 == 3:
+        print('You have selected Lessons.  This part of the program is yet to be written')
+    else:
+        print('Invalid selection')
+
+print('\nWelcome to Dance Diary!')
+
+main_menu()
--- src/student.py
+++ src/student.py
@@ -37,31 +37,29 @@ def add_student():
     mobile = input("Enter student's mobile phone number: ")
     # Create new instance of Student using user input
     new_student = Student(name, email, mobile)
-    
-    # Print confirmation message
-    print((f'You have added a new student with the following details:\nName: {new_student.name}\nEmail: {new_student.email}\nMobile: {new_student.mobile}'))
-    
     # Store new student details in dicitonary format
     new_student_dict = {
         'name': name,
         'email': email,
         'mobile': mobile
     }
-    print(new_student_dict)
 
-    # Store student details dictionary in list and export to .json file
+    # Load existing students.json data & add new student data
+    with open('students.json') as f:
+        students = json.load(f)
+        students.append(new_student_dict)
+    # Output updated data back to students .json
     with open('students.json', 'w') as f:
-        students = [new_student_dict]
         json.dump(students, f, indent=4)
 
-
-    # with open('students.json', 'w') as f:
-        # students = json.load(f)
-        # students.append(new_student_dict)
-        # print(students)
-        # json.dump(students, f, indent=4)
+    # Print confirmation message displaying new student details and total number of students now stored
+    print('\nYou have added a new student with the following details:\n')
+    print(f'Name: {new_student.name}\nEmail: {new_student.email}\nMobile: {new_student.mobile}')
+    print(f'\nTotal Number of Students: {len(students)}')
 
 def view_students():
+    # View details of all current students
     with open('students.json') as f:
         students = json.load(f)
         print(students)
+    
