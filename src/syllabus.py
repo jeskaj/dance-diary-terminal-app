bf2761e03f4c508d103507f17135f59b0e6075ef
jeskaj <jwjakeman@yahoo.com.au>
2024-05-07 13:35:06 +1000

    Wrote function to convert initial syllabus data from .csv to .json; Worked on function to view syllabus data - incomplete

--- src/syllabus.py
+++ src/syllabus.py
@@ -1,9 +1,19 @@
-import csv
+import csv, json
 
-with open('syllabus.csv') as f:
-    reader = csv.DictReader(f)
-    syllabus_list = []
-    for row in reader:
-        # print(row)
-        syllabus_list.append(row)
-    print(syllabus_list)
+def import_csv_syllabus(filename):
+    # Function to input .csv of syllabus data and transform into a list of dictionaries
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        syllabus_list = []
+        for row in reader:
+            syllabus_list.append(row)
+        return(syllabus_list)
+    
+def create_json_syllabus(syllabus_list_of_dicts):
+    # function to output list of dictionaries as .json file
+    with open('syllabus.json', 'w') as f:
+        json.dump(syllabus_list_of_dicts, f, indent=4)
+
+new_syllabus = import_csv_syllabus('syllabus.csv')
+
+create_json_syllabus(new_syllabus)
