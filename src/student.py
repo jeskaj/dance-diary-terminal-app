29e023a1519d6c752717371ee965ac1732af9ae3
jeskaj <jwjakeman@yahoo.com.au>
2024-05-08 18:34:48 +1000

    Finished update student functionality to include updating mobile.  Started added validation to Add Student function to ensure student with name entered does not already exist.

--- src/student.py
+++ src/student.py
@@ -36,61 +36,78 @@ def add_student():
     """
     # Get student details via user input
     print('\n*** CREATE NEW STUDENT ***')
-    name = input("Enter student's full name: ")
-    email = input("Enter student's email: ")
-    mobile = input("Enter student's mobile phone number: ")
-    # Create new instance of Student using user input
-    new_student = Student(name, email, mobile)
-    # Store new student details in dicitonary format
-    new_student_dict = {
-        'name': name,
-        'email': email,
-        'mobile': mobile
-    }
-
-    # Load existing students.json data
+    name = input("Enter student's full name (or 0 to quit): ")
+    # Check a student with name entered does not already exist
     with open('students.json') as f:
         students = json.load(f)
-        # Append new student data to existing data
-        students.append(new_student_dict)
-    # Output updated data back to students.json
-    with open('students.json', 'w') as f:
-        json.dump(students, f, indent=4)
+    # Create list of student names
+    student_names = [name['name'].lower() for name in students]
+    
+    while name != '0':
+        # Check if name input is in list of existing student names
+        name_valid = False
+        while name_valid == False:
+            if name.lower() in student_names:
+                name = input(f"\n*** ERROR:  Student with name {name} already exists.  Please enter a different name, or 0 to cancel:  ")
+                print(name)
+                print(name == '0')
+            else:
+                name_valid = True
 
-    # Print confirmation message displaying new student details and total number of students now stored
-    print('\nYou have added a new student with the following details:\n')
-    print(f'Name: {new_student.name}\nEmail: {new_student.email}\nMobile: {new_student.mobile}')
-    print(f'\nYour total number of students is now: {len(students)}')
+        email = input("Enter student's email: ")
+        mobile = input("Enter student's mobile phone number: ")
+        # Create new instance of Student using user input
+        new_student = Student(name, email, mobile)
+        # Store new student details in dicitonary format
+        new_student_dict = {
+            'name': name,
+            'email': email,
+            'mobile': mobile
+        }
+
+        # Load existing students.json data
+        with open('students.json') as f:
+            students = json.load(f)
+            # Append new student data to existing data
+            students.append(new_student_dict)
+        # Output updated data back to students.json
+        with open('students.json', 'w') as f:
+            json.dump(students, f, indent=4)
+
+        # Print confirmation message displaying new student details and total number of students now stored
+        print('\nYou have added a new student with the following details:\n')
+        print(f'Name: {new_student.name}\nEmail: {new_student.email}\nMobile: {new_student.mobile}')
+        print(f'\nYour total number of students is now: {len(students)}')
 
 
 def select_student():
     # Find and return a single valid student name
     with open('students.json') as f:
         students = json.load(f)
-        # Sort students by name
-        students_sorted = sorted(students, key=lambda d: d['name'])
-        # Create list of student names
-        student_names = [name['name'].lower() for name in students_sorted]
-        # Create variable to track whether or not student name has been found
-        student_found = False
+    # Sort students by name
+    students_sorted = sorted(students, key=lambda d: d['name'])
+    # Create list of student names
+    student_names = [name['name'].lower() for name in students_sorted]
+    # Create variable to track whether or not student name has been found
+    student_found = False
+    # Get student name from user
+    name = input('Enter name of student:  ')
+    # Check if name input is in list of student names
+    if name.lower() in student_names:
+        student_found = True
+    while student_found == False:
+        print(f"\n*** ERROR:  There is no student named {name}.  Please enter a name from the following list:  ***\n")
+        for student in students_sorted:
+            print(student['name'])
         # Get student name from user
-        name = input('Enter name of student:  ')
-        # Check if name input is in list of student names
+        name = input('\nEnter the name of a student from the above list:  ')
         if name.lower() in student_names:
             student_found = True
-        while student_found == False:
-            print(f"\n*** ERROR:  There is no student named {name}.  Please enter a name from the following list:  ***\n")
-            for student in students_sorted:
-                print(student['name'])
-            # Get student name from user
-            name = input('\nEnter the name of a student from the above list:  ')
-            if name.lower() in student_names:
-                student_found = True
-        return name
+    return name
 
 
 def print_student_details(student: dict):
-    print(f"STUDENT: {student['name']}")
+    print(f"\nSTUDENT: {student['name']}")
     print(f"Email: {student['email']}")
     print(f"Mobile: {student['mobile']}")
 
@@ -129,6 +146,7 @@ def view_students():
     
     # If a entered, display all students
     if selection == 'a':
+        print('\nSTUDENT CONTACT DETAILS:')
         for student in students_sorted:
             print_student_details(student)
     # If s entered, run function to find & display details of single student
@@ -149,7 +167,6 @@ def update_student():
             print_student_details(student)
     # Print UPDATE STUDENT DETAILS menu
     print('\nUPDATE STUDENT DETAILS')
-    print('[n]  Update name')
     print('[e]  Update email')
     print('[m]  Update mobile')
     print('[0]  Return to Student Menu')
@@ -159,7 +176,7 @@ def update_student():
     # Unless user entered 0 to return to previous menu, check input is valid
     while selection != '0':
         # Instruct user to try again, until they enter a valid selection
-        while selection != 'n' and selection != 'e' and selection != 'm':
+        while selection != 'e' and selection != 'm':
             selection = input("Invalid input. Enter 'n', 'e', or 'm' (see Menu options above):  ")
             # Exit loop if 0 is entered
             if selection == '0':
@@ -170,25 +187,26 @@ def update_student():
             for student in students:
                 if student['name'].lower() == name.lower():
                     student_found = True
-                    if selection == 'n':            
-                        print(f"Name: {student['name']}")
-                    elif selection == 'e':
+                    # If e entered, update value of email and set relevant confirmation msg
+                    if selection == 'e':
                         new_email = input('Enter new email:  ')
                         student['email'] = new_email
-                        print(f"{student['name']}'s email will be updated to: {student['email']}")
+                        confirmation_msg = f"{student['name']}'s email has been updated to: {student['email']}"
+                    # If m entered, update value of mobile and set relevant confirmation msg
                     elif selection == 'm':
-                        # new_email = input('Enter new email:  ')
-                        # student['email'] = new_email
-                        # print(f"{student['name']}'s email will be updated to: {student['email']}")
-                        print(f"Mobile: {student['mobile']}\n")
+                        new_mobile = input('Enter new mobile:  ')
+                        student['mobile'] = new_mobile
+                        confirmation_msg = f"{student['name']}'s mobile has been updated to: {student['mobile']}"
 
             # Output updated data back to students .json
             with open('students.json', 'w') as f:
                 json.dump(students, f, indent=4)
+            print(confirmation_msg)
         break
         
 
 if __name__ == '__main__':
     # view_students()
     # view_student_deatils()
-    update_student()
+    # update_student()
+    pass
